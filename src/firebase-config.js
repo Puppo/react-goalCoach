78863810afd7c15560d1276448466753ab36ed73
Luca Del Puppo <Puppo@users.noreply.github.com>
2018-01-20 14:50:28 +0100

    Delete firebase-config.js

--- src/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBKCQaDsu7wmwu66Fj9VW3_LQxqSAhfPBw",
-  authDomain: "goalcoach-27096.firebaseapp.com",
-  databaseURL: "https://goalcoach-27096.firebaseio.com",
-  projectId: "goalcoach-27096",
-  storageBucket: "",
-  messagingSenderId: "156007816813"
-};
